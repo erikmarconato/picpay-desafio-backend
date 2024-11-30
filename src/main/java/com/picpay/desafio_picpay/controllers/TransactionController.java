726d5fa22207bb02e39c530eea66e5034be929d4
erikmarconato <erik.marconato@hotmail.com>
2024-11-30 11:29:09 -0300

    feat: implementado Swagger para documentação

--- src/main/java/com/picpay/desafio_picpay/controllers/TransactionController.java
+++ src/main/java/com/picpay/desafio_picpay/controllers/TransactionController.java
@@ -2,6 +2,9 @@ package com.picpay.desafio_picpay.controllers;
 
 import com.picpay.desafio_picpay.dtos.TransactionDto;
 import com.picpay.desafio_picpay.services.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,16 +18,35 @@ public class TransactionController {
     @Autowired
     TransactionService transactionService;
 
+
+    @Operation(summary = "Cria uma nova transação.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transação criada com sucesso."),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor.")
+    })
     @PostMapping
     public String createTransaction (@RequestBody TransactionDto transactionDto){
         return transactionService.createTransaction(transactionDto);
     }
 
+
+    @Operation(summary = "Lista todas as transações realizadas.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sucesso ao listar todas as transações."),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor.")
+    })
     @GetMapping
     public List<TransactionDto> listAllTransactions (){
         return transactionService.listAllTransactions();
     }
 
+
+    @Operation(summary = "Busca a transação por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sucesso ao buscar a transação."),
+            @ApiResponse(responseCode = "404", description = "Transação não encontrada com o ID em questão."),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor.")
+    })
     @GetMapping("/{id}")
     public Optional<TransactionDto> listTransactionById (@PathVariable Long id){
         return transactionService.listTransactionById(id);
--- src/main/java/com/picpay/desafio_picpay/controllers/UserController.java
+++ src/main/java/com/picpay/desafio_picpay/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.picpay.desafio_picpay.controllers;
 
 import com.picpay.desafio_picpay.dtos.UserDto;
 import com.picpay.desafio_picpay.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +20,38 @@ public class UserController {
     @Autowired
     UserService userService;
 
+
+    @Operation(summary = "Cria um novo usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso."),
+            @ApiResponse(responseCode = "409", description = "CPF/CNPJ já cadastrado no sistema"),
+            @ApiResponse(responseCode = "415", description = "Tipo de usuário inválido."),
+            @ApiResponse(responseCode = "422", description = "Email já cadastrado no sistema."),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor.")
+    })
     @PostMapping
     public ResponseEntity<UserDto> createCommonUser (@RequestBody UserDto userDto){
         return userService.createUser(userDto);
     }
 
+
+    @Operation(summary = "Lista todos os usuários cadastrados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sucesso ao listar todos os usuários."),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor.")
+    })
     @GetMapping
     public List<UserDto> listAllUsers (){
         return userService.listAllUsers();
     }
 
+
+    @Operation(summary = "Busca o usuário por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sucesso ao buscar o usuário."),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado com o ID em questão."),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor.")
+    })
     @GetMapping("/{id}")
     public Optional<UserDto> listUserById (@PathVariable Long id){
         return userService.listUserById(id);
